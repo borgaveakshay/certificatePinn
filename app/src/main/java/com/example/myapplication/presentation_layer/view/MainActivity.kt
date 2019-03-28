8f805f10fc1639f8affa52d2174807e13e3c3ff7
sachinsandbhor <sachin.sandbhor@infostretch.com>
2019-03-28 13:20:14 +0530

    1. Adapter and recyclerview added. 2. Data populated in recyclerview.

--- app/src/main/java/com/example/myapplication/presentation_layer/view/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation_layer/view/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.myapplication.presentation_layer.view
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.presentation_layer.models.DataItem
 import com.example.myapplication.presentation_layer.viewmodels.UserViewModel
 import com.example.myapplication.presentation_layer.viewmodels.factory.ViewModelFactory
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 
 
@@ -16,13 +21,21 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
 
     private lateinit var mUserViewModel: UserViewModel
     private val mViewModelFactory: ViewModelFactory by inject()
+    lateinit var userListAdapter: UserListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mUserViewModel = ViewModelProviders.of(this, mViewModelFactory).get(UserViewModel::class.java)
+        initViews()
+    }
 
+    private fun initViews() {
+        userListAdapter = UserListAdapter()
+        avatarList.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        avatarList.setHasFixedSize(true)
+        avatarList.adapter = userListAdapter
     }
 
     override fun onResume() {
@@ -34,7 +47,7 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
             }
             else {
                 it?.let {
-
+                        userListAdapter.updateList(it.data as List<DataItem>)
                         Toast.makeText(this, "Response received!!!", Toast.LENGTH_LONG).show()
                 }
             }
---
+++ app/src/main/java/com/example/myapplication/presentation_layer/view/UserListAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication.presentation_layer.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import com.example.myapplication.presentation_layer.models.DataItem
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.user_row.view.*
+
+class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
+
+    var dataList = mutableListOf<DataItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.user_row, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(dataList[position])
+    }
+
+
+    fun updateList(list: List<DataItem>) {
+        if (list.isNotEmpty()) {
+            dataList.clear()
+            dataList.addAll(list)
+            notifyDataSetChanged()
+        }
+    }
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(data: DataItem) {
+            with(itemView) {
+                var fullname = data.firstName + " " + data.lastName
+                name.text = fullname
+                Picasso.get().load(data.avatar).fit().into(avatar)
+            }
+        }
+    }
+}
